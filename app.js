f64d03388bd1ed43382e4ce820abe8a8fd9d05f0
Zircoz <dixit.setu@gmail.com>
2018-07-24 05:30:46 +0530

    Restructure

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Server listening on port");
 });
+
+app.use("/instructor/register", (req, res) => {
+  res.sendfile(__dirname + "/public/registerInstructor.html");
+});
+
+app.use("/instructor/Login", (req, res) => {
+  res.sendfile(__dirname + "/public/loginpage/instructorLogin.html");
+});
+
+app.use("/instructor/dashboard", (req, res) => {
+  res.sendfile(__dirname + "/public/dashboard/instructorDashboard.html");
+});
